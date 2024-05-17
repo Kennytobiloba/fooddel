a36380952b3a1281db4f10741acd5f9a245ca482
kennytobiloba <olaatunbikehinde@gmail.com>
2024-05-17 03:44:00 +0100

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {assets} from '../../assets/assets'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
